71c4083feb7523502067211041ed3f79329561ea
mt-perform <mtperform2946@icloud.com>
2022-08-06 23:52:04 +0900

    slash-commands

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const discord = require('discord.js')
-const fs = require('fs');
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, Collection, Intents } = require('discord.js');
-require('dotenv').config();
-const prefix = process.env.PREFIX;
+const dotenv = require('dotenv');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+dotenv.config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
   client.commands.set(command.data.name, command);
 }
-
 
 client.once('ready', () => {
   console.log('Ready!');
 });
+
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
@@ -31,9 +35,5 @@ client.on('interactionCreate', async interaction => {
     await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
   }
 });
-
-
-
-
 
 client.login(process.env.DISCORD_TOKEN); 
